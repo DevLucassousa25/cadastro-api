bfa97b079e59a1ea8d973c1e1a27d3c6d2eda280
DevLucassousa25 <devlucasousa@gmail.com>
2025-11-20 13:30:34 -0300

    feat: Coloando o Lombok na calasse pra abstracao

--- src/main/java/dev/java10x/CadastroAPI/Model/Endereco.java
+++ src/main/java/dev/java10x/CadastroAPI/Model/Endereco.java
@@ -1,12 +1,18 @@
 package dev.java10x.CadastroAPI.Model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Table(name = "tb_endereco")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Endereco {
 
     @Id
@@ -23,61 +29,4 @@ public class Endereco {
     @ManyToMany(mappedBy = "enderecos")
     private Set<Pessoa> pessoas = new HashSet<>();
 
-    public Endereco() {}
-
-    public Endereco(String rua, int numero, String cidade, String estado, String cep) {
-        this.rua = rua;
-        this.numero = numero;
-        this.cidade = cidade;
-        this.estado = estado;
-        this.cep = cep;
-    }
-
-    public String getRua() {
-        return rua;
-    }
-
-    public void setRua(String rua) {
-        this.rua = rua;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
-
-    public String getCidade() {
-        return cidade;
-    }
-
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public String getCep() {
-        return cep;
-    }
-
-    public void setCep(String cep) {
-        this.cep = cep;
-    }
-
-    public Set<Pessoa> getPessoas() {
-        return pessoas;
-    }
-
-    public void setPessoas(Set<Pessoa> pessoas) {
-        this.pessoas = pessoas;
-    }
 }
--- src/main/java/dev/java10x/CadastroAPI/Model/Pessoa.java
+++ src/main/java/dev/java10x/CadastroAPI/Model/Pessoa.java
@@ -1,12 +1,18 @@
 package dev.java10x.CadastroAPI.Model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Pessoa {
 
     @Id
@@ -28,80 +34,4 @@ public class Pessoa {
             inverseJoinColumns = @JoinColumn(name = "endereco_id")
     )
     private Set<Endereco> enderecos = new HashSet<>();
-
-    public Pessoa() {}
-
-    public Pessoa(String nome, String sobrenome, String cpf, String rg, String genero, String email, String telefone) {
-        this.nome = nome;
-        this.sobrenome = sobrenome;
-        this.cpf = cpf;
-        this.rg = rg;
-        this.genero = genero;
-        this.email = email;
-        this.telefone = telefone;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getSobrenome() {
-        return sobrenome;
-    }
-
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getRg() {
-        return rg;
-    }
-
-    public void setRg(String rg) {
-        this.rg = rg;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public Set<Endereco> getEnderecos() {
-        return enderecos;
-    }
-
-    public void setEnderecos(Set<Endereco> enderecos) {
-        this.enderecos = enderecos;
-    }
 }
