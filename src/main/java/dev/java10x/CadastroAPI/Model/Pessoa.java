54ea56825474a4779f8d44b61a340b2a2bb2eebf
DevLucassousa25 <devlucasousa@gmail.com>
2025-11-19 22:28:54 -0300

    Cria uma entidade no banco de dadaos aprtir do Pessoa Model

---
+++ src/main/java/dev/java10x/CadastroAPI/Model/Pessoa.java
@@ -0,0 +1,87 @@
+package dev.java10x.CadastroAPI.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+    private  String nome;
+    private  String sobrenome;
+    private  String cpf;
+    private  String rg;
+    private  String genero;
+    private  String email;
+    private  String telefone;
+
+    public Pessoa() {}
+
+    public Pessoa(String nome, String sobrenome, String cpf, String rg, String genero, String email, String telefone) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.cpf = cpf;
+        this.rg = rg;
+        this.genero = genero;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
